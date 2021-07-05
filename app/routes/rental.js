cce4b5f75f21961c6faeb89566fd8c189630e954
prabanjanraja <prabanjan.m@outlook.com>
2021-07-05 22:50:58 +0530

    ember data support added

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,22 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
 export default class RentalRoute extends Route {
-    async model(params) {
-        let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-        let { data } = await response.json();
-
-        let { id, attributes } = data;
-        let type;
-
-        if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-            type = 'Community';
-        } else {
-            type = 'Standalone';
-        }
-
-        console.log({ id, type, ...attributes });
-        return { id, type, ...attributes };
+    @service store;
+    async model() {
+        return this.store.findAll('rental');
     }
 }
